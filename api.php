f357013ea1419b917e3c7ad22350d6cabe293b05
StefanRaluca <raluca.bubulina@yahoo.com>
2024-04-17 15:05:45 +0200

    add axios request and json_string in api

--- api.php
+++ api.php
@@ -1,3 +1,15 @@
 <?php
+// leggo il contenuto del file json
+$json_string = file_get_contents('dischi.json');
+//var_dump($json_string);
 
+
+// decodifico il contenuto in un array assocciativo
+$albums = json_decode($json_string, true);
+//var_dump($albums);
+
+// invio il contenuto come json
+
+header('Content-Type: application/json');
+echo $json_string;
 ?>
--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,7 @@ createApp({
     data() {
 
         return {
-
+            albums: [],
         }
     },
     methods: {
@@ -11,6 +11,16 @@ createApp({
 
     },
     mounted() {
+        // richiesta al nostro backend per ottenere i dati dei dischi
+        axios.get('api.php')
+            .then(response => {
+                //console.log(this.albums);
+                // quando abbiamo i dati li assegniamo alla variabile albums 
+                this.albums = response.data;
+            })
+            .catch(error => {
+                console.error('Errore!', error);
+            });
 
 
     }
